9e67d44455c9ef01bf49d63d39df3b4b9da40205
meryt.matlik <meryt.matlik@vikk.ee>
2025-03-11 16:04:30 +0200

    Versioon ilma ninata

---
+++ Lumememm - Meryt.py
@@ -0,0 +1,16 @@
+import pygame
+pygame.init()
+screen=pygame.display.set_mode([300,300])
+pygame.display.set_caption("Lumememm- Meryt")
+pygame.draw.circle(screen, [255, 255, 255], [150,70], 30,0)
+pygame.draw.circle(screen, [255, 255, 255], [150,138], 40,0)
+pygame.draw.circle(screen, [255, 255, 255], [150,226], 50,0)
+pygame.draw.circle(screen, [0, 0, 0], [140,65], 5,0)
+pygame.draw.circle(screen, [0, 0, 0], [160,65], 5,0)
+pygame.display.update()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
