e7fa8c33d493854e2995bee683fa04d48622b7bb
meryt <meryt.matlik@vikk.ee>
2025-03-11 21:49:37 +0200

    Ilma jutumullita

--- 2.py
+++ 2.py
@@ -5,12 +5,12 @@ screen = pygame.display.set_mode([640, 480])  # Loob akna suurusega 640x480 piks
 pygame.display.set_caption("2")  # Määrab akna pealkirja
 
 # Laeb pildid
-background = pygame.image.load("bg_shop.png")
-seller = pygame.image.load("seller.png")
-chat = pygame.image.load("chat.png")
+background = pygame.image.load("bg_shop.png")#laeb taustapildi
+seller = pygame.image.load("seller.png")#laeb müüja pildi
+chat = pygame.image.load("chat.png")#laeb jutumulli
 
 # Muudab piltide suurust
-seller = pygame.transform.scale(seller, [255, 310])
+seller = pygame.transform.scale(seller, [255, 310])#
 chat = pygame.transform.scale(chat, [150, 150])
 
 # Joonistab pildid õigesti järjestikku
