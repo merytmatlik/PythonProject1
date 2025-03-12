13a1ed445615e536206c5e1638db5b6d5b2cda43
meryt <meryt.matlik@vikk.ee>
2025-03-12 17:50:09 +0200

    Ilma jutumullita

--- 2.py
+++ 2.py
@@ -10,13 +10,18 @@ seller = pygame.image.load("seller.png")#laeb müüja pildi
 chat = pygame.image.load("chat.png")#laeb jutumulli
 
 # Muudab piltide suurust
-seller = pygame.transform.scale(seller, [255, 310])#
-chat = pygame.transform.scale(chat, [150, 150])
+seller = pygame.transform.scale(seller, [255, 304])#muudab müüja pildi suurust
+chat = pygame.transform.scale(chat, [257, 200])#muudab jutumulli suurust
 
 # Joonistab pildid õigesti järjestikku
 screen.blit(background, (0, 0))  # Kõigepealt taust
-screen.blit(seller, [105, 155])  # Siis müüja
-screen.blit(chat, [200, 100])  # Siis vestlusmull (erinev asukoht, et mitte katta müüjat)
+screen.blit(seller, [105, 160])  # Siis müüja
+screen.blit(chat, [245, 70])  # Siis vestlusmull (erinev asukoht, et mitte katta müüjat)
+
+#lisame teksti
+font = pygame.font.SysFont('arial', 24)
+text = font.render("Tere! Olen Meryt!", True, [255,255,255])
+screen.blit(text, [280,140])
 
 pygame.display.update()  # Uuendab ekraani
 
