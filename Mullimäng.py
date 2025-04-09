ebfe517dc10f571f4435c4533844913a6c78a51a
meryt <meryt.matlik@vikk.ee>
2025-04-09 20:46:23 +0300

    Versioon 1

--- Mullimäng.py
+++ Mullimäng.py
@@ -1,5 +1,6 @@
 import pygame
 import sys     #impordib süsteemimooduli, et saaks kasutada sys.exit()
+import random  #lisatud juhuslike värvide genereerimiseks
 
 # Algväärtused
 pygame.init()  #käivitab kõik vajalikud Pygame moodulid
@@ -9,9 +10,8 @@ pygame.display.set_caption("Mullimäng")  #määrab akna nime
 
 # Värvid
 BACKGROUND_COLOR = (173, 216, 230)  #määrab taustavärvi (helesinine)
-CIRCLE_COLOR = (0, 0, 255)  #määrab ringide värvi (sinine)
 
-# Ringi info (koordinaadid + raadius)
+# Ringi info (koordinaadid + raadius + värv)
 circles = []  # list kõigi ringide kohta (asukoht ja raadius)
 radius = 5  #algne ringi raadius
 
@@ -21,8 +21,8 @@ while running:
     screen.fill(BACKGROUND_COLOR)  #täidab ekraani taustavärviga
 
     # Joonistab kõik olemasolevad ringid
-    for pos, r in circles:
-        pygame.draw.circle(screen, CIRCLE_COLOR, pos, r, 1)  #joonistab ringi etteantud positsiooni ja raadiusega
+    for pos, r, color in circles:
+        pygame.draw.circle(screen, color, pos, r, 1)  #joonistab ringi etteantud positsiooni ja raadiusega
 
     # Kontrollib kõiki sündmusi (nt. hiireklikk või akna sulgemine)
     for event in pygame.event.get():
@@ -31,7 +31,8 @@ while running:
 
         elif event.type == pygame.MOUSEBUTTONDOWN:  #kui kasutaja klõpsab hiirega
             pos = pygame.mouse.get_pos()  #saab kliki asukoha (x, y)
-            circles.append((pos, radius))  #lisab uue ringi andmed listi
+            color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))  #genereerib juhusliku värvi
+            circles.append((pos, radius, color))  #lisab uue ringi andmed listi
             radius += 2  #suurendab järgmise ringi raadiust
 
     pygame.display.flip()  #värskendab ekraani, et muudatused oleks nähtavad
